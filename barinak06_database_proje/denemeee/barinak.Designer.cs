a087369669b0117641f4fcb5fea039c2d4a43ff3
zeynep13bayraktar <zeybaybm@gmail.com>
2024-12-23 01:34:25 +0300

    hayvan barınağı
    veri tabanı ve  veri tabanında işlem yapabilen barınak csharp programı

---
+++ barinak06_database_proje/denemeee/barinak.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace denemeee
+{
+    partial class barinak
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_ID = new System.Windows.Forms.TextBox();
+            this.txt_mail = new System.Windows.Forms.TextBox();
+            this.txt_tel = new System.Windows.Forms.TextBox();
+            this.txt_soyad = new System.Windows.Forms.TextBox();
+            this.txt_ad = new System.Windows.Forms.TextBox();
+            this.txt_adres = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_list = new System.Windows.Forms.Button();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_guncelle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_ara = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_ID
+            // 
+            this.txt_ID.Location = new System.Drawing.Point(146, 49);
+            this.txt_ID.Name = "txt_ID";
+            this.txt_ID.Size = new System.Drawing.Size(100, 22);
+            this.txt_ID.TabIndex = 0;
+            // 
+            // txt_mail
+            // 
+            this.txt_mail.Location = new System.Drawing.Point(146, 248);
+            this.txt_mail.Name = "txt_mail";
+            this.txt_mail.Size = new System.Drawing.Size(100, 22);
+            this.txt_mail.TabIndex = 1;
+            // 
+            // txt_tel
+            // 
+            this.txt_tel.Location = new System.Drawing.Point(146, 196);
+            this.txt_tel.Name = "txt_tel";
+            this.txt_tel.Size = new System.Drawing.Size(100, 22);
+            this.txt_tel.TabIndex = 2;
+            // 
+            // txt_soyad
+            // 
+            this.txt_soyad.Location = new System.Drawing.Point(146, 147);
+            this.txt_soyad.Name = "txt_soyad";
+            this.txt_soyad.Size = new System.Drawing.Size(100, 22);
+            this.txt_soyad.TabIndex = 3;
+            // 
+            // txt_ad
+            // 
+            this.txt_ad.Location = new System.Drawing.Point(146, 98);
+            this.txt_ad.Name = "txt_ad";
+            this.txt_ad.Size = new System.Drawing.Size(100, 22);
+            this.txt_ad.TabIndex = 4;
+            // 
+            // txt_adres
+            // 
+            this.txt_adres.Location = new System.Drawing.Point(146, 300);
+            this.txt_adres.Name = "txt_adres";
+            this.txt_adres.Size = new System.Drawing.Size(100, 22);
+            this.txt_adres.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "KİŞİ ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "AD:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "SOYAD:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "TELEFON:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "EMAİL:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 306);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "ADRES:";
+            // 
+            // btn_list
+            // 
+            this.btn_list.Location = new System.Drawing.Point(22, 357);
+            this.btn_list.Name = "btn_list";
+            this.btn_list.Size = new System.Drawing.Size(75, 23);
+            this.btn_list.TabIndex = 12;
+            this.btn_list.Text = "Listele";
+            this.btn_list.UseVisualStyleBackColor = true;
+            this.btn_list.Click += new System.EventHandler(this.btn_list_Click);
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Location = new System.Drawing.Point(133, 407);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(75, 23);
+            this.btn_sil.TabIndex = 13;
+            this.btn_sil.Text = "Sil";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            this.btn_sil.Click += new System.EventHandler(this.btn_sil_Click);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(133, 357);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 14;
+            this.btn_add.Text = "Ekle";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_guncelle
+            // 
+            this.btn_guncelle.Location = new System.Drawing.Point(22, 407);
+            this.btn_guncelle.Name = "btn_guncelle";
+            this.btn_guncelle.Size = new System.Drawing.Size(75, 23);
+            this.btn_guncelle.TabIndex = 15;
+            this.btn_guncelle.Text = "Güncelle";
+            this.btn_guncelle.UseVisualStyleBackColor = true;
+            this.btn_guncelle.Click += new System.EventHandler(this.btn_guncelle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(350, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(438, 379);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // txt_ara
+            // 
+            this.txt_ara.Location = new System.Drawing.Point(243, 357);
+            this.txt_ara.Name = "txt_ara";
+            this.txt_ara.Size = new System.Drawing.Size(75, 23);
+            this.txt_ara.TabIndex = 18;
+            this.txt_ara.Text = "Ara";
+            this.txt_ara.UseVisualStyleBackColor = true;
+            this.txt_ara.Click += new System.EventHandler(this.txt_ara_Click);
+            // 
+            // barinak
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_ara);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_guncelle);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.btn_list);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_adres);
+            this.Controls.Add(this.txt_ad);
+            this.Controls.Add(this.txt_soyad);
+            this.Controls.Add(this.txt_tel);
+            this.Controls.Add(this.txt_mail);
+            this.Controls.Add(this.txt_ID);
+            this.Name = "barinak";
+            this.Text = "barinak";
+            this.Load += new System.EventHandler(this.barinak_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_ID;
+        private System.Windows.Forms.TextBox txt_mail;
+        private System.Windows.Forms.TextBox txt_tel;
+        private System.Windows.Forms.TextBox txt_soyad;
+        private System.Windows.Forms.TextBox txt_ad;
+        private System.Windows.Forms.TextBox txt_adres;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_list;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_guncelle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button txt_ara;
+    }
+}
---
+++ barinak06_database_proje/denemeee/barinak.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Npgsql;
+
+namespace denemeee
+{
+    public partial class barinak : Form
+    {
+        public barinak()
+        {
+            InitializeComponent();
+        }
+
+        private void barinak_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        NpgsqlConnection baglanti = new NpgsqlConnection("server=localHost; port=5432; Database=barinak06; user ID=postgres; password=Zb96104019");
+
+        private void btn_list_Click(object sender, EventArgs e)
+        {
+            string sorgu = "Select * from kisi";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, baglanti);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut1 = new NpgsqlCommand("insert into kisi (kisiID, ad, soyad, telefon, email, adres) values (@p1, @p2, @p3, @p4, @p5, @p6)", baglanti);
+            komut1.Parameters.AddWithValue("@p1", int.Parse(txt_ID.Text));
+            komut1.Parameters.AddWithValue("@p2", txt_ad.Text);
+            komut1.Parameters.AddWithValue("@p3", txt_soyad.Text);
+            komut1.Parameters.AddWithValue("@p4", txt_tel.Text);
+            komut1.Parameters.AddWithValue("@p5", txt_mail.Text);
+            komut1.Parameters.AddWithValue("@p6", txt_adres.Text);
+            komut1.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("kişi ekleme işlemi başarılı bir şekilde gerçekleşti...");
+        }
+
+        private void btn_sil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut2 = new NpgsqlCommand("Delete From kisi where kisiID=@p1", baglanti);
+            komut2.Parameters.AddWithValue("@p1", int.Parse(txt_ID.Text));
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kişi başarılı bir şekilde silindi...");
+
+        }
+
+        private void btn_guncelle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut3 = new NpgsqlCommand("update kisi set ad=@p1, soyad=@p2, telefon=@p3, email=@p4, adres=@p5 where kisiID=@p6",baglanti);
+            komut3.Parameters.AddWithValue("@p1", txt_ad.Text);
+            komut3.Parameters.AddWithValue("@p2", txt_soyad.Text);
+            komut3.Parameters.AddWithValue("@p3", txt_tel.Text);
+            komut3.Parameters.AddWithValue("@p4", txt_mail.Text);
+            komut3.Parameters.AddWithValue("@p5", txt_adres.Text);
+            komut3.Parameters.AddWithValue("@p6", int.Parse(txt_ID.Text));
+            komut3.ExecuteNonQuery();
+            MessageBox.Show("kişi bilgileri başarıyla güncellendi...");
+            baglanti.Close();
+        }
+
+        private void txt_ara_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            string sorgu = "Select * from kisi where (kisiID=@p1 or @p1 IS NULL) and (ad=@p2 or @p2 IS NULL) and (soyad=@p3 or @p3 IS NULL) and (telefon=@p4 or @p4 IS NULL) and (email=@p5 or @p5 IS NULL) and (adres=@p6 or @p6 IS NULL)";
+            NpgsqlCommand komut4 = new NpgsqlCommand(sorgu, baglanti);
+            komut4.Parameters.AddWithValue("@p1", string.IsNullOrWhiteSpace(txt_ID.Text) ? (object)DBNull.Value : int.Parse(txt_ID.Text));
+            komut4.Parameters.AddWithValue("@p2", string.IsNullOrWhiteSpace(txt_ad.Text) ? (object)DBNull.Value : txt_ad.Text);
+            komut4.Parameters.AddWithValue("@p3", string.IsNullOrWhiteSpace(txt_soyad.Text) ? (object)DBNull.Value : txt_soyad.Text);
+            komut4.Parameters.AddWithValue("@p4", string.IsNullOrWhiteSpace(txt_tel.Text) ? (object)DBNull.Value : txt_tel.Text);
+            komut4.Parameters.AddWithValue("@p5", string.IsNullOrWhiteSpace(txt_mail.Text) ? (object)DBNull.Value : txt_mail.Text);
+            komut4.Parameters.AddWithValue("@p6", string.IsNullOrWhiteSpace(txt_adres.Text) ? (object)DBNull.Value : txt_adres.Text);
+
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(komut4);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            baglanti.Close();
+
+        }
+    }
+}
